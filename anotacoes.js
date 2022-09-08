5626a4c0953f357d549ca4db8d54be8aed00419c
ivanilsonsillva <ivanilsonsillva2020@gmail.com>
2022-09-08 08:43:04 -0300

    intro-a-js

---
+++ anotacoes.js
@@ -0,0 +1,31 @@
+/*
+javascript - client - - side
+
+html é onde eu coloco meu código em javascript
+
+js e popular em mais 90% das paginas da internet.
+
+css é onde estilizo a minha pagina html
+
+javascript e onde eu dou funcionalidade para minha pagina
+
+console.log("") tem a mesma funcao escreva do portugol
+
+--------------------
+
+DOM - document object model
+e la que eu consugo estruturar todo o meu documento.
+
+O HTML e o "QUE". o DOM e o "COMO". 
+
+---------------
+VARIAVEIS EM JAVASCRIPT
+
+existe 3 formas simples de se declarar variaveis em js
+
+VAR e LET = para variaveis que mudam com o tempo
+
+CONST = para variaveis que serao sempre constantes
+tipagem dinamica = o js consegue reconhecer automaticamente o tipo
+de cada variavel, sem que eu precise especificar nos minimos detalhes.
+*/
